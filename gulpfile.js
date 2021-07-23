e4b61071b0fafa23bd2647efc45e816276a8f812
Jose Figueroa <jose.figueroa442@gmail.com>
2021-07-23 12:35:18 +0200

    config scss

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Use dart-sass for @use
+//sass.compiler = require('dart-sass');
+const config = {
+	fontsDir: 'dist/fonts/poppins/',
+	jsDir: 'dist/js/',
+	cssDir: 'dist/css/'
+
+}
+function fontTask(){
+	return src('./node_modules/typeface-poppins/files/*.{woff, woff2}')
+		.pipe(dest(config.fontsDir));
+}
+
+// Sass Task
+function scssTask() {
+	return src('app/scss/style.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest(config.cssDir, { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask() {
+	return src('app/js/*.js', { sourcemaps: true })
+		.pipe(babel())
+		.pipe(terser())
+		.pipe(dest(config.jsDir, { sourcemaps: '.' }));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: './dist/',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	cb();
+}
+function browserSyncReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+// Watch Task
+function watchTask() {
+	watch('dist/*.html', browserSyncReload);
+	watch(
+		['app/scss/**/*.scss', 'app/**/*.js'],
+		series(fontTask, scssTask, jsTask, browserSyncReload)
+	);
+}
+
+// Default Gulp Task
+exports.default = series(fontTask, scssTask, jsTask, browserSyncServe, watchTask);
